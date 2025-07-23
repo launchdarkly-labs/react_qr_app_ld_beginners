e3719b9eeeb6800a824aa64d95e1403387dff704
Dina Muscanell <devopsdina@gmail.com>
2025-07-23 10:38:33 -0400

    revert qrCode file back to original

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -1,17 +1,19 @@
 import QRCode from "react-qr-code";
 import { withLDConsumer } from "launchdarkly-react-client-sdk";
 
-//Change QRURL to the URL where you'll be hosting this app
-const QRURL = "https://launchdarkly-labs.github.io/react_qr_app_ld_beginners/";
+// No more hard-coding: just use the current URL
+let QR_URL = document.location.toString();
+
 const qrCodeHome = ({ flags }) => {
 
-    // See this page for details: https://docs.launchdarkly.com/sdk/client-side/react/react-web#flag-keys
-    return flags.showQrCode ? (
+  // The React SDK automatically converts flag keys with dashes and periods to camelCase.
+  // See this page for details: https://docs.launchdarkly.com/sdk/client-side/react/react-web#flag-keys
+  return flags.showQrCode ? (
     <div>
       <br />
       <span style={{ color: 'black' }}><center>Scan me!</center></span>
       <div className="qr-wrapper">
-        <QRCode value={QRURL} />
+        <QRCode value={QR_URL} />
       </div>
     </div>
   ) : (
